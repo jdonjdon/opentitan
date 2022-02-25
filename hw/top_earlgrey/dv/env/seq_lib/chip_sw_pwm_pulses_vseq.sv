2951ed0655287d5cfd05d840feaae1304f8e02e1
Jaedon Kim <jdonjdon@google.com>
2022-02-25 15:03:47 +0000

    [dv,top,pwm] Add initial place holders for pwm test
    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
    
    Add pwm place holder part2
    
    [dv,top,pwm] Add initial place holder for pwm test
    
    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
    
    Add pwm place holder part2
    
    update pr 1111 after review
    
    Signed-off-by: Jaedon Kim <jdonjdon@google.com>

---
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_pwm_pulses_vseq.sv
@@ -0,0 +1,16 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class chip_sw_pwm_pulses_vseq extends chip_sw_base_vseq;
+  `uvm_object_utils(chip_sw_pwm_pulses_vseq)
+
+  `uvm_object_new
+ 
+  virtual task body();
+    super.body();
+    
+    `uvm_info(`gfn, "Not implemented", UVM_LOW)  
+  endtask // body
+   
+endclass // chip_sw_pwm_pulses_vseq
