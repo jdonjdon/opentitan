71198146f45ae06d9792ce37afad799384ba5b67
Jaedon Kim <jdonjdon@google.com>
2022-03-25 19:43:18 +0000

    [dv,clkmgr] sec cm testplan
    Signed-off-by: Jaedon Kim <jdonjdon@google.com>

--- hw/ip/clkmgr/dv/env/clkmgr_env_pkg.sv
+++ hw/ip/clkmgr/dv/env/clkmgr_env_pkg.sv
@@ -120,6 +120,19 @@ package clkmgr_env_pkg;
   };
 
   // functions
+  function void print_hintable(hintables_t tbl);
+    foreach (tbl[i]) begin
+      `uvm_info("HINTBL",$sformatf("entry%0d : %b",i,tbl[i]),UVM_NONE)
+    end
+  endfunction // print_hintable
+
+  function void print_mubi_hintable(mubi_hintables_t tbl);
+    string val = "INVALID";
+    foreach (tbl[i]) begin
+      if(tbl[i].name !="") val = tbl[i].name;
+      `uvm_info("MUBIHINTBL",$sformatf("entry%0d : %s",i,val),UVM_NONE)
+    end
+  endfunction // print_hintable
 
   // package sources
   `include "clkmgr_env_cfg.sv"
